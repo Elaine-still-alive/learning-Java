81f5defeb649d15c62eed3e2548cb0da6294a821
Elaine-still-alive <56043757+Elaine-still-alive@users.noreply.github.com>
2020-11-26 21:07:42 +0300

    Lab2

---
+++ Point3D_2/src/com/company/Main.java
@@ -0,0 +1,51 @@
+package com.company;
+import java.util.*;
+public class Main {
+    public static double computeArea(Point3d point1, Point3d point2, Point3d point3) {
+        double side1, side2,side3;
+        side1 = point1.distanceTo(point2);
+        side2 = point2.distanceTo(point3);
+        side3 = point3.distanceTo(point1);
+        double p = (side1+side2+side3)/2;
+        double area = Math.sqrt(p*(p-side1)*(p-side2)*(p-side3));
+        return area;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in).useLocale(Locale.ENGLISH);
+        Point3d point1 = new Point3d();
+        Point3d point2 = new Point3d();
+        Point3d point3 = new Point3d();
+        System.out.print("Введите координаты первой точки: ");
+        String s1 = in.nextLine();
+        System.out.print("Введите координаты второй точки: ");
+        String s2 = in.nextLine();
+        System.out.print("Введите координаты третьей точки: ");
+        String s3 = in.nextLine();
+        String[] s11 = s1.split(" ");
+        String[] s22 = s2.split(" ");
+        String[] s33 = s3.split(" ");
+        point1.setX(Double.parseDouble(s11[0]));
+        point1.setY(Double.parseDouble(s11[1]));
+        point1.setZ(Double.parseDouble(s11[2]));
+        point2.setX(Double.parseDouble(s22[0]));
+        point2.setY(Double.parseDouble(s22[1]));
+        point2.setZ(Double.parseDouble(s22[2]));
+        point3.setX(Double.parseDouble(s33[0]));
+        point3.setY(Double.parseDouble(s33[1]));
+        point3.setZ(Double.parseDouble(s33[2]));
+        if (point1.comparison(point2) || point2.comparison(point3) || point1.comparison(point3)) {
+            System.out.print("Вы ввели несколько идентичных точек");
+        }
+        else {
+            double res = computeArea(point1, point2, point3);
+            if (Double.isNaN(res)) {
+                System.out.println("Такого треугольника не существует");
+            }
+            else {
+                System.out.println("Площадь треугольника равна " + res);
+            }
+        }
+
+    }
+}
---
+++ Point3D_2/src/com/company/Point3d.java
@@ -0,0 +1,30 @@
+package com.company;
+public class Point3d extends Point2d {
+    //координата Z
+    private double zCoord;
+    //Конструктор инициализации
+    public Point3d(double z){
+        zCoord = z;
+    }
+    //конструктор по умолчанию
+    public Point3d(){
+        this(0);
+    }
+    //возвращение координаты Z
+    public double getZ(){
+        return zCoord;
+    }
+    //установка значения координаты Z
+    public void setZ(double val) {
+        zCoord = val;
+    }
+    //сравнение
+    public boolean comparison(Point3d point){
+        return this.getZ()==point.getZ()&&this.getY()==point.getY()&&this.getX()==point.getX();
+    }
+    // Подсчёт расстояния между двумя точками
+    public Double distanceTo(Point3d point){
+        double distance = Math.sqrt(Math.pow(this.getX() - point.getX(), 2) + Math.pow(this.getY() - point.getY(), 2) + Math.pow(this.getZ() - point.getZ(), 2));
+        return (double) Math.round(distance * 100) / 100;
+    }
+}
